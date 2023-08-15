4ff10b5a135685772668f1b5e2397536c17895b4
elevenstand7 <elevenstand7@gmail.com>
2023-08-15 09:03:46 -0400

    finished hw

---
+++ w8d2/practice-for-ch-js-intro-js-problems1-main/w8d2.js
@@ -0,0 +1,7 @@
+const madLib = (v,a,n)=>{
+    let vew_v = v.toLocaleUpperCase;
+    let vew_a = a.toLocaleUpperCase;
+    let vew_n = n.toLocaleUpperCase;
+    console.log(`We shall ${vew_v}the ${vew_a} ${vew_n}`)
+}
+madLib('make', 'best', 'guac');
